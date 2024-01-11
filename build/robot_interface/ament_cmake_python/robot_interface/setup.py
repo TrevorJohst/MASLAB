d4f0b9f8ce4d5d6844b8c7fb5c1bd3fdde1a920d
TrevorJohst <122303295+TrevorJohst@users.noreply.github.com>
2024-01-11 10:12:55 -0500

    Delete build directory

--- build/robot_interface/ament_cmake_python/robot_interface/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages
-from setuptools import setup
-
-setup(
-    name='robot_interface',
-    version='0.0.0',
-    packages=find_packages(
-        include=('robot_interface', 'robot_interface.*')),
-)
--- build/robot_interface/robot_interface__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/team-2/robot_ws/build/robot_interface/robot_interface__py
-# Build directory: /home/team-2/robot_ws/build/robot_interface/robot_interface__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
